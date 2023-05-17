f68d02e48c15fab475c03bc9c011511464c3aec4
Anbarasu <anbarasun123@gmail.com>
2023-05-17 23:50:53 +0530

    fix: cors

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import io
 from flask import Flask, request, jsonify
 from predict import predict
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/', methods=['POST'])
+@cross_origin()
 def main():
     if request.method == 'POST':
         f = request.files['file']
